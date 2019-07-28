b75f9ff93b64432e7e68e1be2e292155947ffd61
aaaaninja <keshigomu71505@gmail.com>
2019-07-28 17:00:13 +0900

    :tada: lodashを入れてみた......

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
+import _ from "lodash";
+//import R from "ramda";
+
+
+
 const hoge = "aaaaaaaa";
 
 console.log("hoeeeeeeeeee");
+console.log(_.chunk([1,2,34,5,6,7,8,8,9,3,4,7], 3));
 
 [...Array(5)].map((): void => console.log(hoge));
